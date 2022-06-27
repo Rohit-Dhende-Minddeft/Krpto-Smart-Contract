59dcfcac0066c14cf1dbb3485859d306ca28a425
Rohit-Dhende-Minddeft <rohit.dhende@minddeft.com>
2022-06-28 00:18:10 +0530

    Created First Contract of Validation of whether user has installed metamask and whether he/she has connected his/her wallet account

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+// https://eth-rinkeby.alchemyapi.io/v2/tMdOJzyiH9m3B9gtj9w3OAWp3WgXbCG4
+
+require('@nomiclabs/hardhat-waffle');
+ 
+module.exports={
+  solidity: '0.8.0',
+  networks:{
+    rinkeby:{
+      url:'https://eth-rinkeby.alchemyapi.io/v2/tMdOJzyiH9m3B9gtj9w3OAWp3WgXbCG4',
+      accounts:['ac4657e0bcead2e3ee1e8c26927767eeacbccae59c278e3abd495ce62b8d034e']
+    }
+  }
+}
