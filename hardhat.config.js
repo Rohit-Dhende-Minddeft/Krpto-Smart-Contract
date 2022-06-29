97157256c85a62e7ba2463ac2f0f23ce7fd219cf
Rohit-Dhende-Minddeft <rohit.dhende@minddeft.com>
2022-06-29 17:24:10 +0530

    minor changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,6 +8,10 @@ module.exports={
     rinkeby:{
       url:'https://eth-rinkeby.alchemyapi.io/v2/tMdOJzyiH9m3B9gtj9w3OAWp3WgXbCG4',
       accounts:['ac4657e0bcead2e3ee1e8c26927767eeacbccae59c278e3abd495ce62b8d034e']
+    },
+    goerli:{
+      url:'https://eth-goerli.alchemyapi.io/v2/52mLa5SFSjq7wibeGgn8vfkdWzcJoLnW',
+      accounts:['ac4657e0bcead2e3ee1e8c26927767eeacbccae59c278e3abd495ce62b8d034e']
     }
-  }
+  } 
 }
